f939cf86d62eaf0b11de42c78945df74da7370eb
Alena Zavgorodniaia <alzania@yandex.ru>
2022-07-14 13:02:51 +0300

    changed path to redoc

--- api_yamdb/api_yamdb/urls.py
+++ api_yamdb/api_yamdb/urls.py
@@ -8,7 +8,7 @@ urlpatterns = [
     path('auth/', include('django.contrib.auth.urls')),
     path(
         'redoc/',
-        TemplateView.as_view(template_name='redoc.yaml'),
+        TemplateView.as_view(template_name='redoc.html'),
         name='redoc'
     ),
 ]
